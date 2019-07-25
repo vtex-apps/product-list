1fa5334104efc47db0a4769b76a7fd15d1f61c3f
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-07-25 19:05:02 -0300

    Cleanup template code

--- react/ProductList.tsx
+++ react/ProductList.tsx
@@ -1,45 +1,9 @@
-import React, { useState } from 'react'
-import { Input } from 'vtex.styleguide'
+import React, { FunctionComponent, useState } from 'react'
 
-import TranslatedTitle from './components/TranslatedTitle'
-
-import styles from './styles.css'
-
-const Example: StorefrontFunctionComponent<ExampleProps> = ({ title }) => {
-  const [inputValue, setValue] = useState<string | null>(null)
-
+const ProductList: FunctionComponent<any> = () => {
   return (
-    <div className={`${styles.container} flex flex-column pv6 ph4`}>
-      <TranslatedTitle title="Hello from Product List!" />
-      <div className="t-body pv4">
-        <span>Foobar</span>
-      </div>
-      <Input
-        value={inputValue}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setValue(e.target.value)
-        }
-      />
-    </div>
+    <div>Hello from Product List!</div>
   )
 }
 
-interface ExampleProps {
-  title?: string
-}
-
-Example.schema = {
-  title: 'editor.base-store-component.title',
-  description: 'editor.base-store-component.description',
-  type: 'object',
-  properties: {
-    title: {
-      title: 'editor.base-store-component.title.title',
-      description: 'editor.base-store-component.title.description',
-      type: 'string',
-      default: null,
-    },
-  },
-}
-
-export default Example
+export default ProductList
