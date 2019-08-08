7f289cd32ab30bbf0a01bf128c8494a741f08926
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-08-08 16:04:59 -0300

    Implement product list

--- react/ProductList.tsx
+++ react/ProductList.tsx
@@ -1,9 +1,31 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, { FunctionComponent } from 'react'
 
-const ProductList: FunctionComponent<any> = () => {
-  return (
-    <div>Hello from Product List!</div>
-  )
+import ListItem from './components/ListItem'
+
+interface Props {
+  currency: string
+  items: Item[]
+  onQuantityChange: (index: number, value: number) => void
+  onRemove: (index: number) => void
 }
+
+const ProductList: FunctionComponent<Props> = ({
+  currency,
+  items,
+  onQuantityChange,
+  onRemove,
+}) => (
+  <div>
+    {items.map((item: any, index: number) => (
+      <ListItem
+        key={index}
+        currency={currency}
+        item={item}
+        onQuantityChange={(value: number) => onQuantityChange(index, value)}
+        onRemove={() => onRemove(index)}
+      />
+    ))}
+  </div>
+)
 
 export default ProductList
