8aaf0070173f4b25c4b47bbd9bf63d61d38e19fe
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-08-13 13:35:47 -0300

    Fix lint error

--- react/__mocks__/mockItems.ts
+++ react/__mocks__/mockItems.ts
@@ -6,7 +6,7 @@ export const mockItems: Item[] = [
     id: '1',
     detailUrl: '/work-shirt/p',
     imageUrl:
-    'http://storecomponents.vteximg.com.br/arquivos/ids/155476-55-55/Frame-4.jpg?v=636793808441900000',
+      'http://storecomponents.vteximg.com.br/arquivos/ids/155476-55-55/Frame-4.jpg?v=636793808441900000',
     listPrice: 2800000,
     measurementUnit: 'un',
     name: 'قميص العمل الأعلى',
@@ -15,7 +15,7 @@ export const mockItems: Item[] = [
     quantity: 3,
     sellingPrice: 2400000,
     skuName: 'Test SKU 0',
-    variations: [],
+    skuSpecifications: [],
   },
   {
     additionalInfo: {
@@ -24,7 +24,7 @@ export const mockItems: Item[] = [
     id: '30',
     detailUrl: '/long-sleeve-shirt/p',
     imageUrl:
-    'http://storecomponents.vteximg.com.br/arquivos/ids/155487-55-55/Frame-7.jpg?v=636793837686400000',
+      'http://storecomponents.vteximg.com.br/arquivos/ids/155487-55-55/Frame-7.jpg?v=636793837686400000',
     listPrice: 945000,
     measurementUnit: 'un',
     name: '上品なサングラス',
@@ -33,7 +33,7 @@ export const mockItems: Item[] = [
     quantity: 1,
     sellingPrice: 945000,
     skuName: 'Test SKU 1',
-    variations: [],
+    skuSpecifications: [],
   },
   {
     additionalInfo: {
@@ -42,7 +42,7 @@ export const mockItems: Item[] = [
     id: '2000535',
     detailUrl: '/classy--sunglasses/p',
     imageUrl:
-    'http://storecomponents.vteximg.com.br/arquivos/ids/155469-55-55/Frame-8.jpg?v=636793757498800000',
+      'http://storecomponents.vteximg.com.br/arquivos/ids/155469-55-55/Frame-8.jpg?v=636793757498800000',
     listPrice: 400000,
     measurementUnit: 'un',
     name: 'กางเกงขาสั้น St Tropez',
@@ -51,6 +51,6 @@ export const mockItems: Item[] = [
     quantity: 4,
     sellingPrice: 360000,
     skuName: 'Test SKU 2',
-    variations: [],
+    skuSpecifications: [],
   },
 ]
