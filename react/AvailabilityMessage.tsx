e8c53696d38af6d636169fffdabf2fc449ac53dd
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-09-24 16:05:21 -0300

    Fix message layout

--- react/AvailabilityMessage.tsx
+++ react/AvailabilityMessage.tsx
@@ -1,19 +1,49 @@
-import React, { FunctionComponent } from 'react'
+import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Button } from 'vtex.styleguide'
 
 import { useItemContext } from './components/ItemContext'
 import { AVAILABLE, CANNOT_BE_DELIVERED } from './constants/Availability'
 
-const AvailabilityMessage: FunctionComponent = () => {
+interface Props {
+  layout: 'cols' | 'rows'
+}
+
+const setContainerLayout = (prop: string) => {
+  switch (prop) {
+    case 'cols':
+      return 'flex justify-between items-center'
+    case 'rows':
+      return ''
+    default:
+      return ''
+  }
+}
+
+const setTextLayout = (prop: string) => {
+  switch (prop) {
+    case 'cols':
+      return 'pa4'
+    case 'rows':
+      return 'pt4 pr4 pb0 pl4'
+    default:
+      return ''
+  }
+}
+
+const AvailabilityMessage: StorefrontFunctionComponent<Props> = ({ layout }) => {
   const {
     item: { availability },
     onRemove,
   } = useItemContext()
 
   return availability !== AVAILABLE ? (
-    <div className="bg-warning--faded br2 flex-m justify-between">
-      <div className="self-center ph4 pt4 pt0-m">
+    <div className={`bg-warning--faded br2 ${setContainerLayout(
+      layout
+    )}`}>
+      <div className={`t-small lh-title ${setTextLayout(
+      layout
+    )}`}>
         {availability === CANNOT_BE_DELIVERED ? (
           <FormattedMessage id="store/product-list.message.cantBeDelivered" />
         ) : (
@@ -27,6 +57,20 @@ const AvailabilityMessage: FunctionComponent = () => {
       </div>
     </div>
   ) : null
+}
+
+AvailabilityMessage.defaultProps = {
+  layout: 'cols',
+}
+
+AvailabilityMessage.schema = {
+  properties: {
+    layout: {
+      type: 'string',
+      default: AvailabilityMessage.defaultProps.layout,
+      isLayout: true,
+    },
+  },
 }
 
 export default AvailabilityMessage
