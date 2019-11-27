d6bb8506acef13e38fdfbe79da55b4348138562b
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-11-27 11:07:09 -0300

    Force mobile skeleton on phone devices

--- react/ProductListWrapper.tsx
+++ react/ProductListWrapper.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ExtensionPoint } from 'vtex.render-runtime'
 import { SizeMe } from 'react-sizeme'
+import { useDevice } from 'vtex.device-detector'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
 
 const MAX_MOBILE_WIDTH = 640
 
@@ -11,10 +13,14 @@ interface Props {
 }
 
 const ProductListWrapper: StorefrontFunctionComponent<Props> = props => {
+  const { loading } = useOrderForm()
+  const { device } = useDevice()
+
   return (
     <SizeMe noPlaceholder={true}>
       {({ size }) =>
-        size.width && size.width < MAX_MOBILE_WIDTH ? (
+        (loading && device === 'phone') ||
+        (size.width && size.width < MAX_MOBILE_WIDTH) ? (
           <ExtensionPoint id="product-list-content-mobile" {...props} />
         ) : (
           <ExtensionPoint id="product-list-content-desktop" {...props} />
