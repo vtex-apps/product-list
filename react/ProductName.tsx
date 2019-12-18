eb289a1a27bf6fcb745815915192c09da3fde960
victorhmp <victor2142@gmail.com>
2019-12-18 18:56:04 -0300

    Add CSS handles to ProductName component

--- react/ProductName.tsx
+++ react/ProductName.tsx
@@ -1,12 +1,16 @@
 import React, { FunctionComponent } from 'react'
 
 import { Loading } from 'vtex.render-runtime'
+import { useCssHandles } from 'vtex.css-handles'
 
 import { useItemContext } from './components/ItemContext'
 import { opaque } from './utils/opaque'
 
+const CSS_HANDLES = ['productName'] as const
+
 const ProductName: FunctionComponent = () => {
   const { item, loading } = useItemContext()
+  const handles = useCssHandles(CSS_HANDLES)
 
   if (loading) {
     return <Loading />
@@ -15,9 +19,9 @@ const ProductName: FunctionComponent = () => {
   return (
     <a
       id={`name-${item.id}`}
-      className={`c-on-base t-title lh-copy fw6 no-underline fw5-m ${opaque(
-        item.availability
-      )}`}
+      className={`c-on-base t-title lh-copy fw6 no-underline fw5-m ${
+        handles.productName
+      } ${opaque(item.availability)}`}
       href={item.detailUrl}
     >
       {item.name}
