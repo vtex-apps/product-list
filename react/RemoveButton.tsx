5f5e6987f8bf396242fcde46f2d2cbe33452d893
victorhmp <victor2142@gmail.com>
2019-12-18 19:06:54 -0300

    Add CSS handles to RemoveButton component

--- react/RemoveButton.tsx
+++ react/RemoveButton.tsx
@@ -1,23 +1,31 @@
 import React, { FunctionComponent } from 'react'
 import { IconDelete } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
 
 import { Loading } from 'vtex.render-runtime'
 
 import { useItemContext } from './components/ItemContext'
 import { opaque } from './utils/opaque'
 
+const CSS_HANDLES = ['removeButtonContainer', 'removeButton'] as const
+
 const RemoveButton: FunctionComponent = () => {
   const { item, loading, onRemove } = useItemContext()
+  const handles = useCssHandles(CSS_HANDLES)
 
   if (loading) {
     return <Loading />
   }
 
   return (
-    <div className={opaque(item.availability)}>
+    <div
+      className={`${handles.removeButtonContainer} ${opaque(
+        item.availability
+      )}`}
+    >
       <button
         id={`remove-button-${item.id}`}
-        className="pointer bg-transparent bn pa2"
+        className={`${handles.removeButton} pointer bg-transparent bn pa2`}
         title="remove"
         onClick={onRemove}
       >
