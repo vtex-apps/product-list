ea8bbd748c95b4d44ab857bf8b909af93baa8992
victorhmp <victor2142@gmail.com>
2019-12-18 18:43:51 -0300

    Add CSS handles to ProductBrand

--- react/ProductBrand.tsx
+++ react/ProductBrand.tsx
@@ -1,12 +1,16 @@
 import React, { FunctionComponent } from 'react'
 
 import { Loading } from 'vtex.render-runtime'
+import { useCssHandles } from 'vtex.css-handles'
 
 import { useItemContext } from './components/ItemContext'
 import { opaque } from './utils/opaque'
 
+const CSS_HANDLES = ['productBrandName'] as const
+
 const ProductBrand: FunctionComponent = () => {
   const { item, loading } = useItemContext()
+  const handles = useCssHandles(CSS_HANDLES)
 
   if (loading) {
     return <Loading />
@@ -15,7 +19,9 @@ const ProductBrand: FunctionComponent = () => {
   return (
     <div
       id={`brand-name-${item.id}`}
-      className={`ttu f7 fw6 c-muted-1 fw5-m ${opaque(item.availability)}`}
+      className={`ttu f7 fw6 c-muted-1 fw5-m ${
+        handles.productBrandName
+      } ${opaque(item.availability)}`}
     >
       {item.additionalInfo.brandName}
     </div>
