f0bb3a2c44437c30510b3335c6eac4a54c499174
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-06 10:43:21 -0300

    add translations

--- react/ProductQuantity.tsx
+++ react/ProductQuantity.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 import { useItemContext } from './components/ItemContext'
 
 const ProductQuantity: React.FC = () => {
   const { item } = useItemContext()
 
-  return <span className="c-muted-1 t-body">{item.quantity} un.</span>
+  return (
+    <span className="c-muted-1 t-body">
+      <FormattedMessage
+        id="store/product-list.quantity-units"
+        values={{ quantity: item.quantity }}
+      />
+    </span>
+  )
 }
 
 export default ProductQuantity
