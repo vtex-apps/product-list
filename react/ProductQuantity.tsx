bbf5cc8e17b024ccf2e15fe2e159e117b0d3e0b0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-06 10:45:54 -0300

    add css handle and update readme

--- react/ProductQuantity.tsx
+++ react/ProductQuantity.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { useCssHandles } from 'vtex.css-handles'
 
 import { useItemContext } from './components/ItemContext'
 
+const CSS_HANDLES = ['productQuantityUnit'] as const
+
 const ProductQuantity: React.FC = () => {
   const { item } = useItemContext()
+  const cssHandles = useCssHandles(CSS_HANDLES)
 
   return (
-    <span className="c-muted-1 t-body">
+    <span className={`${cssHandles.productQuantityUnit} c-muted-1 t-body`}>
       <FormattedMessage
         id="store/product-list.quantity-units"
         values={{ quantity: item.quantity }}
