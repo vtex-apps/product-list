ab0226909a39460371d3e1036a92e02dc378b9d1
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2020-09-01 11:52:38 -0300

    mock added

--- react/__mocks__/vtex.render-runtime.tsx
+++ react/__mocks__/vtex.render-runtime.tsx
@@ -2,3 +2,11 @@ import type { FunctionComponent } from 'react'
 import React from 'react'
 
 export const Loading: FunctionComponent = () => <div />
+
+export const useRuntime = () => {
+  let rootPath = ''
+  const setRootPath = (rP: string) => {
+    rootPath = rP
+  }
+  return { rootPath, setRootPath }
+}
