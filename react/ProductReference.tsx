01e17761ef5241e3395429cc6d1347a4425f7641
Lucas Castro <lucas.castro@acct.global>
2020-10-21 11:11:53 -0300

    Finish new component product-reference

--- react/ProductReference.tsx
+++ react/ProductReference.tsx
@@ -4,17 +4,26 @@ import { useCssHandles } from 'vtex.css-handles'
 import { useItemContext } from './ItemContext'
 import { opaque } from './utils/opaque'
 
-import { PRODUCT_REFERENCE_ID, PRODUCT_SKU_REFERENCE_ID } from './constants/Identifiers'
+import {
+  PRODUCT_REFERENCE_ID,
+  PRODUCT_SKU_REFERENCE_ID,
+  PRODUCT_ID,
+  PRODUCT_SKU_ITEM_ID,
+} from './constants/Identifiers'
 
-const CSS_HANDLES = ['productReferenceId'] as const
+const CSS_HANDLES = [
+  'productReferenceId',
+  'productIdentifierValue',
+  'productIdentifierLabelValue',
+] as const
 
-interface identifierOption {
-  identifierOptionValue?: string | undefined
+interface Props {
+  identifierLabel?: String
+  identifierOption?: String
 }
 
-const ProductReference: FunctionComponent<identifierOption> = ({
-  identifierOptionValue = 'ProductReferenceId',
-}) => {
+const ProductReference: FunctionComponent<Props> = props => {
+  const { identifierOption = PRODUCT_REFERENCE_ID, identifierLabel } = props
   const { item, loading } = useItemContext()
   const handles = useCssHandles(CSS_HANDLES)
 
@@ -22,29 +31,38 @@ const ProductReference: FunctionComponent<identifierOption> = ({
     return <Loading />
   }
 
-  console.log(item)
-
-  const [identifier, setIdentifier] = useState<string | null | undefined>()
-
-  console.log(identifier)
+  const [identifierValue, setIdentifierValue] = useState<
+    String | null | undefined
+  >()
 
   useEffect(() => {
-    if (identifierOptionValue === PRODUCT_REFERENCE_ID) {
-      setIdentifier(item.productRefId)
-    } else if (identifierOptionValue === PRODUCT_SKU_REFERENCE_ID) {
-      setIdentifier(item.refId)
+    if (identifierOption === PRODUCT_REFERENCE_ID) {
+      setIdentifierValue(item.productRefId)
+    } else if (identifierOption === PRODUCT_SKU_REFERENCE_ID) {
+      setIdentifierValue(item.refId)
+    } else if (identifierOption === PRODUCT_SKU_ITEM_ID) {
+      setIdentifierValue(item.id)
+    } else if (identifierOption === PRODUCT_ID) {
+      setIdentifierValue(item.productId)
     }
   }, [item.productRefId])
 
-  return (
+  return identifierValue ? (
     <div
       className={`c-on-base t-title lh-copy fw6 no-underline fw5-m ${
         handles.productReferenceId
       } ${opaque(item.availability)}`}
     >
-      {identifier}
+      {identifierLabel && (
+        <span className={`${handles.productIdentifierLabelValue}`}>
+          {identifierLabel}
+        </span>
+      )}
+      <span className={`${handles.productIdentifierValue}`}>
+        {identifierValue}
+      </span>
     </div>
-  )
+  ) : null
 }
 
 export default ProductReference
--- react/constants/Identifiers.ts
+++ react/constants/Identifiers.ts
@@ -1,2 +1,4 @@
+export const PRODUCT_ID = 'ProductId'
+export const PRODUCT_SKU_ITEM_ID = 'ProductSkuItemId'
 export const PRODUCT_REFERENCE_ID = 'ProductReferenceId'
 export const PRODUCT_SKU_REFERENCE_ID = 'ProductSkuReferenceId'
