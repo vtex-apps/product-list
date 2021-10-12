d77676881a7587ce0c9ac75fce545c17b5647621
Flavia Minutillo <flavia_minutillo_reply@whirlpool.com>
2021-10-12 15:35:22 +0200

    Mock definition for the applyModifiers function

--- react/__mocks__/vtex.css-handles.ts
+++ react/__mocks__/vtex.css-handles.ts
@@ -2,8 +2,63 @@ export const useCssHandles = (cssHandles: string[]) => {
   const handles: Record<string, string> = {}
 
   cssHandles.forEach((handle) => {
-    handles[handle] = handle
+      handles[handle] = handle
   })
 
   return handles
 }
+
+const validateModifier = (modifier: string) => {
+  if (typeof modifier !== 'string') {
+
+      return false
+  }
+
+  /* This is not an error, so doesn't log any message, but should
+   * invalidate the current modifier and not include it */
+  if (modifier === '') {
+      return false
+  }
+
+  if (/[^A-z0-9-]/.test(modifier)) {
+
+      return false
+  }
+
+  return true
+}
+
+export const applyModifiers = (handles: string, modifier: string | string[]) => {
+  const normalizedModifiers =
+      typeof modifier === 'string' ? [modifier] : modifier
+
+  if (!Array.isArray(normalizedModifiers)) {
+      console.error(
+          'Invalid modifier type on `cssHandles.applyModifier`. Please use either a string or an array of strings'
+      )
+
+      return handles
+  }
+
+  const splitHandles = handles.split(' ')
+
+  const modifiedHandles = normalizedModifiers
+      .map((currentModifier) => {
+          const isValid = validateModifier(currentModifier)
+
+          if (!isValid) {
+              return ''
+          }
+
+          return splitHandles
+              .map((handle) => `${handle}--${currentModifier}`)
+              .join(' ')
+              .trim()
+      })
+      .filter((l) => l.length > 0)
+      .join(' ')
+      .trim()
+
+  return splitHandles.concat(modifiedHandles).join(' ').trim()
+}
+
